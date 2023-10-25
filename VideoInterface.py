4102d4e5f81f49907fee023f3c9ae873ee64f512
3anya-T0n1c <khufujason@outlook.com>
2023-10-26 03:22:16 +0800

    完善视频选项卡部分功能
    文件输入输出、编码器、编码参数、压制硬件加速

--- VideoInterface.py
+++ VideoInterface.py
@@ -3,8 +3,11 @@
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QPixmap, QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QFileDialog
+from qfluentwidgets import InfoBarIcon, InfoBar, PushButton, setTheme, Theme, FluentIcon, InfoBarPosition, InfoBarManager
 from functools import partial
 import os
+from threading import Thread
+
 
 from UI.Ui_video import Ui_Video
 
@@ -42,6 +45,20 @@ class VideoInterface(QWidget, Ui_Video):
         self.HeightNum.setDisabled(1)
         self.IfEnableSwitch.checkedChanged.connect(self.ResolutionChange)
 
+        # 开始压制
+        self.StartButton.clicked.connect(self.ProcessFunc)
+        self.StartButton.setWindowIconText("")
+        self.StartButton.windowIconTextChanged.connect(self.ProcessComplte)
+        self.ProgressBar.setVisible(0)
+
+        # 硬件加速
+        self.HardAccler.addItem("软解")
+        self.HardAccler.addItem("Nvidia")
+        self.HardAccler.addItem("AMD")
+        self.HardAccler.addItem("Intel")
+        # CRF不支持硬件编码
+        self.HardAccler.setDisabled(1)
+
     # 文件选择函数
     def FileSelect(self, TargetLine, AutoFill):
         dir = QFileDialog()
@@ -53,7 +70,7 @@ class VideoInterface(QWidget, Ui_Video):
             if AutoFill != 0:
                 FileExt = os.path.splitext(FilePath[0])[1]
                 FilePath = os.path.splitext(FilePath[0])[0]
-                NewFilePath = FilePath + '_output' + FileExt
+                NewFilePath = FilePath + '_output.mp4'
                 AutoFill.setText(NewFilePath)
 
     # 自定义分辨率控制
@@ -74,10 +91,100 @@ class VideoInterface(QWidget, Ui_Video):
             # CRF默认24，小数两位
             self.ParmsNum.setValue(24)
             self.ParmsNum.setDecimals(2)
+            # CRF不支持硬件编码
+            self.HardAccler.setCurrentIndex(0)
+            self.HardAccler.setDisabled(1)
         # 1是VBR模式
         elif(choice == 1):
             self.KbpsLabel.setVisible(1)
             self.ParmsSetTitle.setText("目标比特率")
-            # VBR默认1500，没有小数
-            self.ParmsNum.setValue(1500)
+            # VBR默认5000，没有小数
+            self.ParmsNum.setValue(5000)
             self.ParmsNum.setDecimals(0)
+            # VBR支持硬件编码
+            self.HardAccler.setDisabled(0)
+
+    # 压制控制
+    def ProcessFunc(self):
+        # 地址缺失
+        if(self.InputLine.text() == '' or self.OutputLine.text() == ''):
+            InfoBar.error(
+                title='未定义视频地址',
+                content="请确认你是否已经设定了正确的输入输出视频地址",
+                orient=Qt.Horizontal,
+                isClosable=True,
+                position=InfoBarPosition.BOTTOM_RIGHT,
+                duration=6000,
+                parent=self
+            )
+            return
+
+        ProcessCmd = "tools\\ffmpeg -hide_banner -i \"" + self.InputLine.text() + \
+            "\" -y "
+        ProcessCmd0 = ""
+
+        if(self.EncoderType.text() == 'x264'):
+            if(self.HardAccler.text() == "软解"):
+                ProcessCmd += "-vcodec libx264 "
+            elif(self.HardAccler.text() == "Nvidia"):
+                ProcessCmd += "-vcodec h264_nvenc "
+            elif(self.HardAccler.text() == "AMD"):
+                ProcessCmd += "-vcodec h264_amf "
+            elif(self.HardAccler.text() == "Intel"):
+                ProcessCmd += "-vcodec h264_qsv "
+        else:
+            if(self.HardAccler.text() == "软解"):
+                ProcessCmd += "-vcodec libx265 "
+            elif(self.HardAccler.text() == "Nvidia"):
+                ProcessCmd += "-vcodec hevc_nvenc "
+            elif(self.HardAccler.text() == "AMD"):
+                ProcessCmd += "-vcodec hevc_amf "
+            elif(self.HardAccler.text() == "Intel"):
+                ProcessCmd += "-vcodec hevc_qsv "
+
+        if(self.ButtonCRF.isChecked()):
+            ProcessCmd += "-crf " + str(self.ParmsNum.value()) + " "
+        elif(self.ButtonVBR.isChecked()):
+            ProcessCmd += "-b:v " + str(self.ParmsNum.value()) + "K "
+        else:
+            ProcessCmd0 = ProcessCmd
+            ProcessCmd0 += "-b:v " + \
+                str(self.ParmsNum.value()) + "K -pass 1 -an -f rawvideo -y NUL"
+
+            ProcessCmd += "-b:v " + str(self.ParmsNum.value()) + "K -pass 2 "
+
+        thread_01 = Thread(target=self.CmdThread, args=(ProcessCmd0, ProcessCmd))
+        thread_01.start()
+
+    def CmdThread(self, ProcessCmd0, ProcessCmd):
+        
+        self.ProgressBar.setVisible(1)
+        self.StartButton.setText("正在压制...")
+        self.StartButton.setWindowIconText(" ")
+        self.StartButton.setDisabled(1)
+        
+        if(self.TextLine.text() != ''):
+            ProcessCmd += "-vf \"subtitles=\'" + self.TextLine.text().replace(":","\:") + "\'\" "
+
+        ProcessCmd += "\"" + self.OutputLine.text() + "\""
+        print(ProcessCmd)
+        if(ProcessCmd0 != ""):
+            os.system(ProcessCmd0)
+        os.system(ProcessCmd)
+        
+        self.ProgressBar.setVisible(0)
+        self.StartButton.setText("开始压制")
+        self.StartButton.setDisabled(0)
+        self.StartButton.setWindowIconText("")
+    
+    def ProcessComplte(self):
+        if(self.StartButton.text() == "开始压制"):
+            InfoBar.success(
+                    title='任务执行完成',
+                    content="请确认是否压制成功",
+                    orient=Qt.Horizontal,
+                    isClosable=True,
+                    position=InfoBarPosition.BOTTOM_RIGHT,
+                    duration=6000,
+                    parent=self
+                )
