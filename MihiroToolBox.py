61e790743dcad361e1336a1ab37bee709fcdeda5
3anya-T0n1c <khufujason@outlook.com>
2023-10-26 23:15:02 +0800

    添加了设置界面和启动画面

--- MihiroToolBox.py
+++ MihiroToolBox.py
@@ -1,16 +1,18 @@
 # coding:utf-8
 import sys
+import configparser
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QSplashScreen
 
-from qfluentwidgets import SplitFluentWindow, FluentIcon
+from qfluentwidgets import SplitFluentWindow, FluentIcon, NavigationItemPosition, setTheme, Theme
 
 from VideoInterface import VideoInterface
 from AudioInterface import AudioInterface
 from CommonInterface import CommonInterface
 from PackageInterface import PackageInterface
+from SettingInterface import SettingInterface
 
 
 class MihiroToolBox(SplitFluentWindow):
@@ -18,6 +20,8 @@ class MihiroToolBox(SplitFluentWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('MihiroToolBox')
+        self.setWindowIcon(QIcon('img/logo.png'))
+        self.move(500, 75)
 
         # 添加视频子界面
         self.VideoInterface = VideoInterface(self)
@@ -29,21 +33,52 @@ class MihiroToolBox(SplitFluentWindow):
 
         # 添加通用子界面
         self.CommonInterface = CommonInterface(self)
-        self.addSubInterface(self.CommonInterface, FluentIcon.LABEL, '常用')
-        
+        self.addSubInterface(self.CommonInterface,
+                             FluentIcon.APPLICATION, '常用')
+
         # 添加封装子界面
         self.PackageInterface = PackageInterface(self)
         self.addSubInterface(self.PackageInterface, FluentIcon.MEDIA, '封装')
 
+        # 添加设置子界面
+        self.SettingInterface = SettingInterface(self)
+        self.addSubInterface(
+            self.SettingInterface, FluentIcon.SETTING, '设置', NavigationItemPosition.BOTTOM)
+
 
 if __name__ == '__main__':
+    
+    # 读取配置文件
+    conf = configparser.ConfigParser()
+
+    conf.read('config.ini')
+    theme = conf.get('DEFAULT', 'theme')
+    showSplash = conf.get('DEFAULT', 'splash')
+    
     # 启用高分屏
     QApplication.setHighDpiScaleFactorRoundingPolicy(
         Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
+    # 设置主题
+    if(theme == '0'):
+        setTheme(Theme.AUTO)
+    elif(theme == '1'):
+        setTheme(Theme.LIGHT)
+    elif(theme == '2'):
+        setTheme(Theme.DARK)
+    
     app = QApplication(sys.argv)
+    
+
+    splash = QSplashScreen()
+    splash.setPixmap(QPixmap(r'img/splash.png'))
+    if(showSplash == '1'):
+        splash.show()
+    
     ex = MihiroToolBox()
     ex.show()
+    splash.finish(ex)  # 主界面加载完成后隐藏
+    splash.deleteLater()
     sys.exit(app.exec_())
---
+++ SettingInterface.py
@@ -0,0 +1,59 @@
+# coding:utf-8
+import configparser
+
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtGui import QPixmap, QPainter, QColor
+from PyQt5.QtWidgets import QWidget
+from qfluentwidgets import InfoBarIcon, InfoBar, PushButton, setTheme, Theme, FluentIcon, InfoBarPosition, InfoBarManager
+
+from UI.Ui_setting import Ui_Form
+
+class SettingInterface(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        
+        self.HyperlinkLabel.setUrl('https://github.com/Eanya-Tonic/MihiroToolbox')
+
+        # 选择主题
+        
+        conf = configparser.ConfigParser()
+        conf.read('config.ini')
+        theme = conf.get('DEFAULT', 'theme')
+        
+        self.ThemeBox.addItem('跟随系统')
+        self.ThemeBox.addItem('浅色')
+        self.ThemeBox.addItem('深色')
+        
+        self.ThemeBox.setCurrentIndex(int(theme))
+        self.ThemeBox.currentIndexChanged.connect(self.ThemeBoxChanged)
+        
+        # 关闭开屏画面
+        splash = conf.get('DEFAULT', 'splash')
+        self.LaunchCheck.setChecked(bool(int(splash)))
+        self.LaunchCheck.clicked.connect(self.LaunchCheckClicked)
+        
+    # 选择主题
+    def ThemeBoxChanged(self):
+        conf = configparser.ConfigParser()
+        conf.read('config.ini')
+        conf.set('DEFAULT', 'theme', str(self.ThemeBox.currentIndex()))
+        conf.write(open('config.ini', 'w'))
+        
+        InfoBar.info(
+                title='提示',
+                content="主题修改重启应用后生效",
+                orient=Qt.Horizontal,
+                isClosable=True,
+                position=InfoBarPosition.BOTTOM_RIGHT,
+                duration=5000,
+                parent=self
+            )
+        
+    # 关闭开屏画面
+    def LaunchCheckClicked(self):
+        conf = configparser.ConfigParser()
+        conf.read('config.ini')
+        conf.set('DEFAULT', 'splash', str(int(self.LaunchCheck.isChecked())))
+        conf.write(open('config.ini', 'w'))
+    
